cca63c07b93df252dc9d6c79ac737c743e1d25fb
cmellinas <clement.mellinas@gmail.com>
2019-07-13 16:18:13 +0200

    server - define all constants in separate file

---
+++ server/constants.js
@@ -0,0 +1,11 @@
+const urlBase = 'https://gateway.marvel.com:443/v1/public/';
+const urlBaseCharacter = urlBase + 'characters';
+const apikey = '4c0365c8935578227098db2a1fb33d38';
+const privateApiKey = '2fb8bd96798fac0ad6146f9d828ec7fa8c71d581';
+
+module.exports = Object.freeze({
+  urlBase,
+  urlBaseCharacter,
+  apikey,
+  privateApiKey,
+});
