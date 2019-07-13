172313aef38e5d8a618760bfb3b077e1a7a7562f
cmellinas <clement.mellinas@gmail.com>
2019-07-13 21:48:17 +0200

    server - add cors module

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const config = require('config');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('./routes'));
